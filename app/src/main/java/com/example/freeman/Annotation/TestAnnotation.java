af355238ae5e8927fd7f45af66743c713562c003
freeman <983836292@qq.com>
2016-09-23 10:08:54 -0700

    增加Annotation

---
+++ app/src/main/java/com/example/freeman/Annotation/TestAnnotation.java
@@ -0,0 +1,19 @@
+package com.example.freeman.Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by freeman on 2016/7/28.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TestAnnotation {
+    String name() default "";
+
+    String sex() default "男";
+
+    int age() default 0;
+}
